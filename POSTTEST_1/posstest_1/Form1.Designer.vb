1191864a510a8ea010d4adb4a3b92bfe9336f380
VanDelmonte <ikhwanhariyanto@student.unmul.ac.id>
2026-02-26 16:27:08 +0800

    POSSTEST1
    penambabahan history saat semester di tambah akan ada history penambahannya, dan ada maksimal penambahan ip semester di max di semester 14

---
+++ POSTTEST_1/posstest_1/Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        lblPredikat = New Label()
+        txtIPSemester = New TextBox()
+        txtIPKumulatif = New TextBox()
+        btnTambah = New Button()
+        btnReset = New Button()
+        lstHistory = New ListBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(154, 140)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(86, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "IP Semester"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(151, 193)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(89, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "IP Kumulatif"
+        ' 
+        ' lblPredikat
+        ' 
+        lblPredikat.AutoSize = True
+        lblPredikat.Location = New Point(151, 257)
+        lblPredikat.Name = "lblPredikat"
+        lblPredikat.Size = New Size(53, 20)
+        lblPredikat.TabIndex = 2
+        lblPredikat.Text = "Label3"
+        ' 
+        ' txtIPSemester
+        ' 
+        txtIPSemester.Location = New Point(151, 163)
+        txtIPSemester.Name = "txtIPSemester"
+        txtIPSemester.Size = New Size(200, 27)
+        txtIPSemester.TabIndex = 3
+        ' 
+        ' txtIPKumulatif
+        ' 
+        txtIPKumulatif.Location = New Point(151, 216)
+        txtIPKumulatif.Name = "txtIPKumulatif"
+        txtIPKumulatif.Size = New Size(200, 27)
+        txtIPKumulatif.TabIndex = 4
+        ' 
+        ' btnTambah
+        ' 
+        btnTambah.Location = New Point(146, 288)
+        btnTambah.Name = "btnTambah"
+        btnTambah.Size = New Size(94, 29)
+        btnTambah.TabIndex = 5
+        btnTambah.Text = "Tambah"
+        btnTambah.UseVisualStyleBackColor = True
+        ' 
+        ' btnReset
+        ' 
+        btnReset.Location = New Point(257, 288)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(94, 29)
+        btnReset.TabIndex = 6
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = True
+        ' 
+        ' lstHistory
+        ' 
+        lstHistory.FormattingEnabled = True
+        lstHistory.Location = New Point(372, 139)
+        lstHistory.Name = "lstHistory"
+        lstHistory.Size = New Size(252, 164)
+        lstHistory.TabIndex = 7
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(lstHistory)
+        Controls.Add(btnReset)
+        Controls.Add(btnTambah)
+        Controls.Add(txtIPKumulatif)
+        Controls.Add(txtIPSemester)
+        Controls.Add(lblPredikat)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblPredikat As Label
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Friend WithEvents txtIPSemester As TextBox
+    Friend WithEvents txtIPKumulatif As TextBox
+
+    Private Sub txtIPSemester_TextChanged(sender As Object, e As EventArgs) Handles txtIPSemester.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents lstHistory As ListBox
+End Class
---
+++ POSTTEST_1/posstest_1/Form1.vb
@@ -0,0 +1,66 @@
+﻿Public Class Form1
+    Dim totalIP As Double = 0
+    Dim jumlahSemester As Integer = 0
+    Dim maxSemester As Integer = 14
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        Dim ipSemester As Double
+
+        If jumlahSemester >= maxSemester Then
+            MessageBox.Show("Maksimal 14 semester telah tercapai! Klik Reset untuk mengulang.", "Batas Semester", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtIPSemester.Enabled = False
+            btnTambah.Enabled = False
+            Exit Sub
+        End If
+
+        If Double.TryParse(txtIPSemester.Text, ipSemester) Then
+            If ipSemester < 0 Or ipSemester > 4 Then
+                MessageBox.Show("//IP Semester harus antara 0 dan 4 !!!\\")
+                Exit Sub
+            End If
+
+            totalIP += ipSemester
+            jumlahSemester += 1
+
+            Dim ipk As Double = totalIP / jumlahSemester
+            txtIPKumulatif.Text = Math.Round(ipk, 2).ToString("0.00")
+
+            If ipk >= 3.01 Then
+                lblPredikat.Text = "Sangat Memuaskan (gachor lee)"
+            ElseIf ipk >= 2.76 Then
+                lblPredikat.Text = "Memuaskan (beh dikit lagi gachor)"
+            ElseIf ipk >= 2.0 Then
+                lblPredikat.Text = "Cukup (waduh)"
+            Else
+                lblPredikat.Text = "Di Bawah Standar (kebanyakan main ML)"
+            End If
+
+            lstHistory.Items.Add("Semester " & jumlahSemester & " | IP: " & ipSemester.ToString("0.00"))
+
+            lstHistory.TopIndex = lstHistory.Items.Count - 1
+
+            If jumlahSemester >= maxSemester Then
+                MessageBox.Show("Semester 14 telah diinput. Tidak bisa menambah lagi!", "Batas Tercapai", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                txtIPSemester.Enabled = False
+                btnTambah.Enabled = False
+            End If
+
+            txtIPSemester.Clear()
+            txtIPSemester.Focus()
+        Else
+            MessageBox.Show("Input harus berupa angka!")
+        End If
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        totalIP = 0
+        jumlahSemester = 0
+        txtIPSemester.Clear()
+        txtIPKumulatif.Clear()
+        lblPredikat.Text = ""
+        lstHistory.Items.Clear()
+        txtIPSemester.Enabled = True
+        btnTambah.Enabled = True
+        txtIPSemester.Focus()
+    End Sub
+End Class
